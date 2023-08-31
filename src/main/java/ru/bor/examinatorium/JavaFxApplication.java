0da584064ef172c48516efa2641549ec68064889
BorodinovYuriy <aditone2378@gmail.com>
2023-09-01 00:33:01 +0300

    welcomePage UI

--- src/main/java/ru/bor/examinatorium/JavaFxApplication.java
+++ src/main/java/ru/bor/examinatorium/JavaFxApplication.java
@@ -5,6 +5,7 @@ import javafx.application.Platform;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import lombok.RequiredArgsConstructor;
 import net.rgielen.fxweaver.core.FxWeaver;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -13,7 +14,6 @@ import ru.bor.examinatorium.controllers.WelcomeController;
 
 @Component
 public class JavaFxApplication extends Application {
-
     private ConfigurableApplicationContext applicationContext;
 
     @Override
@@ -31,6 +31,7 @@ public class JavaFxApplication extends Application {
         Parent root = fxWeaver.loadView(WelcomeController.class);
         Scene scene = new Scene(root);
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
--- src/main/java/ru/bor/examinatorium/controllers/WelcomeController.java
+++ src/main/java/ru/bor/examinatorium/controllers/WelcomeController.java
@@ -1,50 +1,60 @@
 package ru.bor.examinatorium.controllers;
 
+
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import lombok.RequiredArgsConstructor;
 import net.rgielen.fxweaver.core.FxWeaver;
 import net.rgielen.fxweaver.core.FxmlView;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Component;
-import ru.bor.examinatorium.JavaFxApplication;
-import ru.bor.examinatorium.WeatherService;
-
-import java.io.IOException;
+import ru.bor.examinatorium.services.WelcomeService;
 
 @Component
 @FxmlView("../welcome-stage.fxml")
+@RequiredArgsConstructor
 public class WelcomeController {
 
-    @FXML
-    public Button test;
-    @FXML
-    private Label weatherLabel;
-    @FXML
-    private Button weather;
-    private WeatherService weatherService;
+    private final WelcomeService welcomeService;
 
-    @Autowired
-    private ApplicationContext context;
+    private final ApplicationContext context;
 
-    @Autowired
-    public WelcomeController(WeatherService weatherService) {
-        this.weatherService = weatherService;
+    @FXML
+    public ListView<String> listViewTestChoice;
+    @FXML
+    public TextField idTextField;
+    @FXML
+    public Button startTestButton;
+    @FXML
+    public CheckBox adminVisibleCheckBox;
+    @FXML
+    public AnchorPane adminLogoAnchorPane;
+    @FXML
+    public TextField adminLogin;
+    @FXML
+    public TextField adminPassword;
+    @FXML
+    public Button adminAccessButton;
+    @FXML
+    public Label choiceTheTestLabel;
+    @FXML
+    public Button testListButton;
+    @FXML
+    public TextArea help;
+
+
+    public void setVisibleAdminPane(ActionEvent actionEvent){
+        help.setVisible(false);
+        adminLogoAnchorPane.setVisible(adminVisibleCheckBox.isSelected());
     }
-
-    public void loadWeatherForecast(ActionEvent actionEvent) {
-        this.weatherLabel.setText(weatherService.getWeatherForecast());
-    }
-
-    public void loadStage_2(ActionEvent actionEvent){
-        test.getScene().getWindow().hide();
+    public void loadMainPage(ActionEvent actionEvent){
+        startTestButton.getScene().getWindow().hide();
 
         Stage stage = new Stage();
         FxWeaver fxWeaver = context.getBean(FxWeaver.class);
@@ -55,16 +65,39 @@ public class WelcomeController {
 
 
     }
+    public void loadAdminPage(ActionEvent actionEvent){
+        startTestButton.getScene().getWindow().hide();
 
+        Stage stage = new Stage();
+        FxWeaver fxWeaver = context.getBean(FxWeaver.class);
+        Parent root = fxWeaver.loadView(AdminController.class);
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void loadViewTestChoice() {
+        listViewTestChoice.
+                getItems().
+                addAll(
+                        "Item 1",
+                        "Item 2",
+                        "Item 3",
+                        "Item 4",
+                        "Item 5",
+                        "Item 6",
+                        "Item 7",
+                        "Item 8",
+                        "Item 9",
+                        "Item 10",
+                        "Item 11",
+                        "Item 12"
+                );
 
-
-
-
-
-
-
-
-
+        testListButton.setVisible(false);
+        choiceTheTestLabel.setVisible(true);
+        listViewTestChoice.setVisible(true);
+        help.setVisible(false);
+    }
 
 
 }
